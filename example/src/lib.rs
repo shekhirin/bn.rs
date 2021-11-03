85f7c7dae808ef265ba6ab5f40d323f12aa2b309
Alexey Shekhirin <a.shekhirin@gmail.com>
2021-11-03 22:59:46 +0300

    bump version to 0.1.2

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,13 +1,15 @@
-use bn_rs::BN;
-use primitive_types::{H160, U128};
 use std::str::FromStr;
+
+use primitive_types::{H160, U128};
 use wasm_bindgen::prelude::*;
+
+use bn_rs::BN;
 
 #[wasm_bindgen]
 pub fn sum(a: BN, b: BN) -> Result<BN, JsValue> {
     // `BNError` implements `Into<JsValue>`, so we can use `?` here
     let a = u128::try_from(a)?; // std uints are supported
-    let b: U128 = b.try_into()?; // primitive-types supported too
+    let b: U128 = b.try_into()?; // primitive-types uints supported too
 
     let result = a + b.as_u128();
 
